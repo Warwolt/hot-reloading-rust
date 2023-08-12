357aeadfcca03a1b3f62956e8fddc200f37c4a4b
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-08-12 22:05:03 +0200

    Fix

--- src/load_app.rs
+++ src/load_app.rs
@@ -10,11 +10,10 @@ pub struct App {
 
 impl App {
     pub fn new(lib_path: &Path) -> Self {
-        let absolute_lib_path = absolute_lib_path(lib_path);
-        let absolute_lib_copy_path = absolute_lib_copy_path(&absolute_lib_path);
-        std::fs::copy(&absolute_lib_path, &absolute_lib_copy_path).unwrap();
+        let (lib_path, lib_copy_path) = get_paths(lib_path);
+        std::fs::copy(&lib_path, &lib_copy_path).unwrap();
 
-        let lib = unsafe { Library::new(&absolute_lib_copy_path).unwrap() };
+        let lib = unsafe { Library::new(&lib_copy_path).unwrap() };
         let update = load_symbol(&lib, "update");
         App { _lib: lib, update }
     }
@@ -24,23 +23,19 @@ impl App {
     }
 }
 
-fn load_symbol<T>(lib: &Library, symbol: &str) -> RawSymbol<T> {
-    unsafe { lib.get::<T>(symbol.as_bytes()).unwrap().into_raw() }
-}
-
-fn absolute_lib_path(relative_lib_path: &Path) -> PathBuf {
+fn get_paths(relative_lib_path: &Path) -> (PathBuf, PathBuf) {
     assert!(relative_lib_path.is_relative());
     let exe_path = env::current_exe().unwrap();
     let exe_dir = exe_path.parent().unwrap();
-    exe_dir.join(relative_lib_path)
+    let lib_path = exe_dir.join(relative_lib_path);
+    let lib_copy_path = exe_dir.join(format!(
+        "{}-0.dll",
+        lib_path.file_stem().unwrap().to_string_lossy()
+    ));
+
+    (lib_path, lib_copy_path)
 }
 
-fn absolute_lib_copy_path(absolute_lib_path: &Path) -> String {
-    assert!(absolute_lib_path.is_absolute());
-    let absolute_lib_copy_path = format!(
-        "{}-0.dll",
-        absolute_lib_path.file_stem().unwrap().to_string_lossy()
-    );
-
-    absolute_lib_copy_path
+fn load_symbol<T>(lib: &Library, symbol: &str) -> RawSymbol<T> {
+    unsafe { lib.get::<T>(symbol.as_bytes()).unwrap().into_raw() }
 }
