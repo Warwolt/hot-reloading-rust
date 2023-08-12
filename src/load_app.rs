cb53251dbc37a8a383b4fff623fb0f3564fdcfba
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-08-12 20:28:04 +0200

    Add helper function to load symbol

--- src/load_app.rs
+++ src/load_app.rs
@@ -9,12 +9,16 @@ pub struct App {
 
 impl App {
     pub fn new<P: AsRef<OsStr>>(lib_path: P) -> Self {
-        let _lib = unsafe { Library::new(lib_path).unwrap() };
-        let update = { unsafe { _lib.get::<fn() -> ()>(b"update").unwrap().into_raw() } };
-        App { _lib, update }
+        let lib = unsafe { Library::new(lib_path).unwrap() };
+        let update = load_symbol(&lib, "update");
+        App { _lib: lib, update }
     }
 
     pub fn update(&self) {
         (self.update)()
     }
+}
+
+fn load_symbol<T>(lib: &Library, symbol: &str) -> RawSymbol<T> {
+    unsafe { lib.get::<T>(symbol.as_bytes()).unwrap().into_raw() }
 }
