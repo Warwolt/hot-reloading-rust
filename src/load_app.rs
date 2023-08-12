d8088b99518aa1b21fa4696f4d7b36979619a0d8
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-08-12 20:08:17 +0200

    Add some basic input handling

--- src/load_app.rs
+++ src/load_app.rs
@@ -1,13 +1,15 @@
+use libloading::os::windows::Symbol as RawSymbol;
+use libloading::Library;
 use std::ffi::OsStr;
 
 pub struct App {
-    _lib: libloading::Library,
-    hello: libloading::os::windows::Symbol<fn() -> ()>,
+    _lib: Library,
+    hello: RawSymbol<fn() -> ()>,
 }
 
 impl App {
     pub fn new<P: AsRef<OsStr>>(lib_path: P) -> Self {
-        let _lib = unsafe { libloading::Library::new(lib_path).unwrap() };
+        let _lib = unsafe { Library::new(lib_path).unwrap() };
         let hello = { unsafe { _lib.get::<fn() -> ()>(b"hello").unwrap().into_raw() } };
         App { _lib, hello }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 mod load_app;
 
+fn key_is_down(key: winapi::ctypes::c_int) -> bool {
+    unsafe { winapi::um::winuser::GetKeyState(key) & 1 << 15 != 0 }
+}
+
 fn main() {
     let app = load_app::App::new("./app.dll");
     app.hello();
+
+    // loop until escape
+    let mut prev_tick = std::time::SystemTime::now();
+    let mut escape_pressed = false;
+    let mut f5_pressed = false;
+    'main: loop {
+        /* Input */
+        let now = std::time::SystemTime::now();
+
+        let escape_pressed_prev = escape_pressed;
+        escape_pressed = key_is_down(winapi::um::winuser::VK_ESCAPE);
+        let escape_pressed_now = !escape_pressed_prev && escape_pressed;
+
+        let f5_pressed_prev = f5_pressed;
+        f5_pressed = key_is_down(winapi::um::winuser::VK_F5);
+        let f5_pressed_now = !f5_pressed_prev && f5_pressed;
+
+        /* Update */
+        if escape_pressed_now {
+            break 'main;
+        }
+
+        if f5_pressed_now {
+            println!("F5 pressed"); // todo, rebuild here
+        }
+
+        if now.duration_since(prev_tick).unwrap().as_millis() > 1000 {
+            prev_tick = now;
+            app.hello(); // make this "update"
+        }
+    }
+
+    println!("Good bye");
 }
