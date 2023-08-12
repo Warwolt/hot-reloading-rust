a442371998091ba54d43cbe8a86c766a7d74b907
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-08-12 22:00:39 +0200

    Cleanup

--- src/load_app.rs
+++ src/load_app.rs
@@ -1,7 +1,7 @@
 use libloading::os::windows::Symbol as RawSymbol;
 use libloading::Library;
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub struct App {
     _lib: Library,
@@ -10,14 +10,8 @@ pub struct App {
 
 impl App {
     pub fn new(lib_path: &Path) -> Self {
-        assert!(lib_path.is_relative());
-        let exe_path = env::current_exe().unwrap();
-        let exe_dir = exe_path.parent().unwrap();
-        let absolute_lib_path = exe_dir.join(lib_path);
-        let absolute_lib_copy_path = format!(
-            "{}-0.dll",
-            absolute_lib_path.file_stem().unwrap().to_string_lossy()
-        );
+        let absolute_lib_path = absolute_lib_path(lib_path);
+        let absolute_lib_copy_path = absolute_lib_copy_path(&absolute_lib_path);
         std::fs::copy(&absolute_lib_path, &absolute_lib_copy_path).unwrap();
 
         let lib = unsafe { Library::new(&absolute_lib_copy_path).unwrap() };
@@ -32,4 +26,21 @@ impl App {
 
 fn load_symbol<T>(lib: &Library, symbol: &str) -> RawSymbol<T> {
     unsafe { lib.get::<T>(symbol.as_bytes()).unwrap().into_raw() }
+}
+
+fn absolute_lib_path(relative_lib_path: &Path) -> PathBuf {
+    assert!(relative_lib_path.is_relative());
+    let exe_path = env::current_exe().unwrap();
+    let exe_dir = exe_path.parent().unwrap();
+    exe_dir.join(relative_lib_path)
+}
+
+fn absolute_lib_copy_path(absolute_lib_path: &Path) -> String {
+    assert!(absolute_lib_path.is_absolute());
+    let absolute_lib_copy_path = format!(
+        "{}-0.dll",
+        absolute_lib_path.file_stem().unwrap().to_string_lossy()
+    );
+
+    absolute_lib_copy_path
 }
