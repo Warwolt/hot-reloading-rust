88e26436ddb86f83d88bc141a7205420f51bfa5e
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-08-12 19:47:39 +0200

    Add wrapper struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::ffi::OsStr;
+
+struct App {
+    _lib: libloading::Library,
+    hello: libloading::os::windows::Symbol<fn() -> ()>,
+}
+
+impl App {
+    fn new<P: AsRef<OsStr>>(lib_path: P) -> Self {
+        let _lib = unsafe { libloading::Library::new(lib_path).unwrap() };
+        let hello = { unsafe { _lib.get::<fn() -> ()>(b"hello").unwrap().into_raw() } };
+        App { _lib, hello }
+    }
+
+    fn hello(&self) {
+        (self.hello)()
+    }
+}
+
 fn main() {
-    let app_dll = unsafe { libloading::Library::new("./app.dll").unwrap() };
-    let hello = unsafe { app_dll.get::<fn() -> ()>(b"hello").unwrap() };
-    hello();
+    let app = App::new("./app.dll");
+    app.hello();
 }
